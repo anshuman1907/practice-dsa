8b37a94bb88454e1a8f1895314ecdba00785d09c
anshumangadha <anshuman@gmail.com>
2025-06-24 10:43:48 +0530

    sync

---
+++ sll.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,item= None, next=None):
+        self.item= item
+        self.next= next
+        
+    
+class Sll:
+    def __init__(self,start=None):
+        self.start= start
+        
+        
+    def is_empty(self):
+        return self.start== None:
+            
+        
+    def insert_at_start(self,data):
+            
+        n= Node(data,self,start)
+        self.start=n
+        
+    def insert_at_last(self,data):
+        n= Node(data)
+        if not self.is_empty
+    
