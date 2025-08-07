0f6032d1645c837de24ef5ade19c79e4ba7a5b17
Anshuman <201937@kit.ac.in>
2025-08-08 02:31:23 +0530

    "hjbhj"

--- pro1/pro.py
+++ pro1/pro.py
@@ -1,12 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,APIRouter, HTTPException
 from database import get_collections
+#  """for database saving permanent"""
+from pydantic import BaseModel
 
 app = FastAPI()
 student_collection = get_collections()
 
+
+class Student(BaseModel):
+    name: str
+    age: int
+    grade: str
+
+
 @app.get("/")
 async def home():
     return list(student_collection.find({}, {"_id": 0}))
+
+
+
+@app.post("/")
+async def create(new:Student):
+    try:
+        res= student_collection.insert_one(dict(new))
+        return {"status_code":200,"id":str(res.inserted_id)}
+    
+    except Exception as e:
+        return HTTPException(status_code=500,detail=f"some error {e}")
 
 
 
