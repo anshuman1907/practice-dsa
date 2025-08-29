044390e427abcc41832881977b95f117f8f0066f
Anshuman <201937@kit.ac.in>
2025-08-29 15:33:52 +0530

    "sync"

---
+++ js/lec9.js
@@ -0,0 +1,47 @@
+// anyonumus function 
+
+
+
+// function statement
+
+function a(){
+    console.log("anshuman")
+}
+// function expresssion
+var b = function () {
+    console.log("b")
+}
+
+
+//  function decaleation. >>>>> // also known as fuction. statement a
+
+
+// anonymous functionn
+
+var a = function (){
+    console.log("anshu")
+}
+
+a()
+
+// named function expresssion
+
+var a = function xyz(){
+    console.log(xyz)
+}
+a()
+
+
+// diff parameter and arugument
+
+
+// var a = function (para1,para2){
+//     console.log("anshu")
+// }
+// a(argu1,argu2)
+
+
+
+
+// first claas function 
+
