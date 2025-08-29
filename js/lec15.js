23583653e12ad7b083e9d7b552aca53d21b60dba
Anshuman <201937@kit.ac.in>
2025-08-29 22:22:34 +0530

    "sync"

--- js/lec15.js
+++ js/lec15.js
@@ -1,1 +1,41 @@
 // map filter redusce 
+
+// map
+
+const arr=[1,2,4,5,3]
+
+function double(x){
+    return x*2
+}
+const output=arr.map(double)
+
+console.log(output)
+
+
+
+function triple(x){
+    return x*3
+}
+const output1=arr.map(triple)
+
+
+console.log(output1)
+
+
+// in Binary
+
+function  binary(x){
+    return x.toString(2)
+
+}
+const output2=arr.map(binary)
+
+console.log(output2)
+
+
+// 2nd methode
+
+
+const output3=arr.map((x) => x.toString(2))
+
+console.log(output3)
